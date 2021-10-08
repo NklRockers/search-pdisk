7715a9f4541163ac32d85fb94a8d01edbaa5f288
NklRockers <90549426+NklRockers@users.noreply.github.com>
2021-10-08 11:42:41 +0530

    Update movielinks.py

--- movielinks.py
+++ movielinks.py
@@ -1,9 +1,8 @@
 
 
 movies_ids = {
-    'till death': ['nv2mr5000zyr'],['nv2mr5000sht'],['nv2mr500099b']
+    'till death':['nv2mr5000zyr', 'nv2mr5000sht' , 'nv2mr500099b' ]
     
-    'till2 death': ['nv2mr5000zyr']['nv2mr5000sht']['nv2mr500099b']
     
     
 }
