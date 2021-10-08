a80718da22deab511d181b91bf1dbbf5cea58a75
NklRockers <90549426+NklRockers@users.noreply.github.com>
2021-10-08 11:21:37 +0530

    Update movielinks.py

--- movielinks.py
+++ movielinks.py
@@ -1,9 +1,8 @@
 
 
 movies_ids = {
-    'till death 400mb': ['nv2mr5000zyr','nv2mr5000sht','nv2mr500099b'],
-    'till death 1gb': ['nv2mr5000sht'],
-    'till death 2gb': ['nv2mr500099b'],
+    'till death': ['nv2mr5000zyr' 'nv2mr5000sht' 'nv2mr500099b'],
+  
     
     
 }
