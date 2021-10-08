d1590e384c3c4cf6a982799223e84a220d4ae99a
NklRockers <90549426+NklRockers@users.noreply.github.com>
2021-10-08 11:02:00 +0530

    Update movielinks.py

--- movielinks.py
+++ movielinks.py
@@ -1,8 +1,7 @@
 
 
 movies_ids = {
-    'sundari': ['nv2kn5002aqp', 'nv2kn5005em4', 'nv2kn5001s8t', 'nv2kn50040uz', 'nv2kn5004zs1', 'nv2ith0001gn',
-                'nv2ith0006b1', 'nv2ith0003xn', 'nv2ith000oke', 'nv2ith000xuy'],
+    'Till Death': ['nv2mr5000zyr', 'nv2mr5000sht', 'Till Death'],
     
     
 }
